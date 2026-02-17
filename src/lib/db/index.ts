93478d1a55e4173f196c43bb0483a5fa3dc4bdeb
Zephyrrr <kuangdadu@gmail.com>
2026-02-17 13:24:50 -0800

    refactor: add return type to `getDb` function

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -4,6 +4,6 @@ import type { DrizzleD1Database } from "drizzle-orm/d1";
 
 export type DB = DrizzleD1Database<typeof schema>;
 
-export function getDb(env: Env) {
+export function getDb(env: Env): DB {
   return drizzle(env.DB, { schema });
 }
